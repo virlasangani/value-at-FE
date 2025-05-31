3681a02d6831f6c1d9d5ce28594b0621c8a52100
viral2323 <viralsangani1920@gmail.com>
2025-05-31 17:26:42 +0530

    Fix image path

--- src/components/Job/JobState.jsx
+++ src/components/Job/JobState.jsx
@@ -1,4 +1,7 @@
 import { FileText, Plus } from 'lucide-react';
+import totalJob from '../../assets/briefcase-3.svg'
+import applicationJob from '../../assets/brifcase icon.svg'
+import hiredJob from '../../assets/handshak icon.svg'
 
 function StatCard({ title, value, icon, bgColor, iconBgColor }){
   return (
@@ -21,21 +24,21 @@ export const JobStats = ({jobsState}) => {
       <StatCard
         title="Total Jobs Posted"
         value={jobsState.totalJobs}
-        icon="/src/assets/briefcase-3.svg"
+        icon={totalJob}
         bgColor="bg-[#1E2544]"
         iconBgColor="bg-[#242C55]"
       />
       <StatCard
         title="Application received"
         value={jobsState.totalApplied}
-        icon="/src/assets/brifcase icon.svg"
+        icon={applicationJob}
         bgColor="bg-[#3D2427]"
         iconBgColor="bg-[#4A2C30]"
       />
       <StatCard
         title="Hired"
         value={jobsState.totalHired}
-        icon="/src/assets/handshak icon.svg"
+        icon={hiredJob}
         bgColor="bg-[#0A2021]"
         iconBgColor="bg-[#0C2628]"
       />
